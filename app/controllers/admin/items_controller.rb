08bb10fecc6b5870eb2b5e0dfb121d5fa333df9e
ottotto3 <xufangyi8@gmail.com>
2024-05-30 09:18:14 +0000

    [Add]エラーメッセージ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,9 +6,12 @@ class Admin::ItemsController < ApplicationController
   
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_root_path
-  end
+    if @item.save
+      redirect_to edit_admin_item_path(@item.id)
+    else
+      render :new
+    end
+  end 
   
   def index
     @items = Item.all
@@ -23,9 +26,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to admin_item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
   
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_many :posts
   
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :capacity, presence: true
+  
   # 投稿画像がなかったときno_imageを表示する
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
